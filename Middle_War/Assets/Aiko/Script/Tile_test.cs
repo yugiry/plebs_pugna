7cc0ee6cd262cda4b1795c4725f3763111710ca0
Aiko-Dornan <h1202415010@hiratagakuen.onmicrosoft.com>
2024-10-24 12:29:18 +0900

    Unity ユニットをクリックしたら攻撃範囲を表すマスが出るようになった(消すことができない)

---
+++ Middle_War/Assets/Aiko/Script/Tile_test.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// ChatGPT-4 2023/5/8
+// 任意の場所にオブジェクトを配置（座標フリー）
+public class Tile_test : MonoBehaviour
+{
+    int i;
+    int j;
+
+    public GameObject circlePrefab;
+    private int count=0;
+    public void ClickDown()
+    {
+        if (Input.GetMouseButtonDown(0)&&count==0)
+        {
+            Vector3 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            //position.z = 0f;
+            circlePrefab.transform.position = new Vector3(i, j, 0);
+            for (i = -80; i < 40; i+=40)//tate
+            {
+                for (j = -80; j < 40; j+=40)//yoko
+                {
+                    //GameObject newCircle = Instantiate(circlePrefab/*, position, Quaternion.identity*/);
+                    GameObject newCircle = Instantiate(circlePrefab, circlePrefab.transform.position, Quaternion.identity);
+                }
+            }
+            // Do something with newCircle here, if needed
+            count++;
+        }
+
+
+    }
+
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0)&&count==0)
+        {
+            //Vector3 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            //position.z = 0f;
+
+            //GameObject newCircle = Instantiate(circlePrefab, position, Quaternion.identity);
+            // Do something with newCircle here, if needed
+
+            ClickDown();
+
+        }
+        if (Input.GetMouseButtonDown(0) && count == 1)
+        {
+            Destroy(this.gameObject);
+            count--;
+        }
+        }
+}
--- Middle_War/Assets/Aiko/Script/Unit_Range.cs
+++ Middle_War/Assets/Aiko/Script/Unit_Range.cs
@@ -5,12 +5,32 @@ using UnityEngine.EventSystems;
 
 public class Unit_Range : MonoBehaviour/*,IPointerClickHandler*/
 {
+   
+
+    public GameObject kougekihanni;
+
     public void OnPointerClick(/*PointerEventData eventdata*/)
     {
         //var eventData = (PointerEventData)data;
 
+        //kougekihanni.transform.position = new Vector3(100, 0, 0);
+        Active_false_selected_by_Tag("Respawn");
+
         Debug.Log(this.name+"クリックされた");
+        //GameObject newBlock = Instantiate(kougekihanni, kougekihanni.transform.position, Quaternion.identity);
         this.gameObject.transform.Find("range tile").gameObject.SetActive(true);
+    }
+
+    public void Active_false_selected_by_Tag(string tag)
+    {
+        // Hierarchy状のタグのついたオブジェクトをすべて格納
+        GameObject[] tags = GameObject.FindGameObjectsWithTag(tag);
+
+        // すべてのタグを非表示にする
+        foreach (GameObject t in tags)
+        {
+            t.SetActive(false);
+        }
     }
 
     // Start is called before the first frame update
