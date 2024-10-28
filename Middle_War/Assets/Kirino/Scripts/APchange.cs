e8fa50f0d434572df7a283796cc12f7fae1db8be
kirino-kazuto <h1202419004@hiratagakuen.onmicrosoft.com>
2024-10-28 09:36:10 +0900

    tesuto

---
+++ Middle_War/Assets/Kirino/Scripts/APchange.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class APchange : MonoBehaviour
+{
+    [SerializeField] Text apText;
+
+    int maxAp = 999;
+    float nowAp = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        apText.text = nowAp.ToString() + "/" + maxAp.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
