f17d82f84fc49c49c7d4bef77c088b5c40f76ed7
Aiko-Dornan <h1202415010@hiratagakuen.onmicrosoft.com>
2024-10-29 12:39:47 +0900

    ユニットの攻撃範囲表示の改造途中。

---
+++ Middle_War/Assets/Aiko/Script/Destroy_Range.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy_Range : MonoBehaviour
+{
+    public void DestroyRange()
+    {
+
+        Destroy(this.gameObject);
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Middle_War/Assets/Aiko/Script/New_range_hyouji.cs
+++ Middle_War/Assets/Aiko/Script/New_range_hyouji.cs
@@ -7,18 +7,43 @@ public class New_range_hyouji : MonoBehaviour
     public GameObject unit_click;
     public Transform parent;
     public GameObject base_point_unit;
-    GameObject unitclick;
+    private GameObject unitclick;
     public void Click_unit()
     {
-         
-        Vector3 pos = parent.transform.localPosition;
+        //unitclick = unit_click as GameObject; 
+
+        Vector3 pos = parent.transform.localPosition;//クリックされたユニットの位置情報
         if ( GameObject.Find("infantry"))
         {
+            Destroy_Range();
+            Destroy_Range();
+            for (float x = - 4.0f; x <= +4.0f; x += 4.0f)
+            {
+                for (float y = -4.0f; y <= +4.0f; y += 4.0f)
+                {
+                    unitclick = Instantiate(unit_click, new Vector3(pos.x+x, pos.y+y, -5.5f), Quaternion.identity, parent) as GameObject;
+                }
 
-            Instantiate(unit_click, new Vector3(pos.x + 4.0f, pos.y + 4.0f, -4.0f), Quaternion.identity,parent);
+            }
+
+            
             Debug.Log("クリックされたぞい。");
-            // Destroy_Range();
-            Destroy(unitclick);
+             
+           // Destroy(unitclick, 1);
+        }else if (GameObject.Find("archer"))
+        {
+            Destroy_Range();
+            Destroy_Range();
+            for (float x = -4.0f; x <= +4.0f; x += 4.0f)
+            {
+                for (float y = -4.0f; y <= +4.0f; y += 4.0f)
+                {
+                    unitclick = Instantiate(unit_click, new Vector3(pos.x + x, pos.y + y, -5.5f), Quaternion.identity, parent) as GameObject;
+                }
+
+            }
+
+
         }
 
 
@@ -27,15 +52,21 @@ public class New_range_hyouji : MonoBehaviour
 
     private void Destroy_Range()
     {
-        //var unitclick = Instantiate(unit_click) as GameObject;
-        Destroy(unitclick);
+        GameObject[] unitclick = GameObject.FindGameObjectsWithTag("Respawn");
 
+        if (unit_click.activeSelf) {
+            //var unitclick = Instantiate(unit_click) as GameObject;
+            foreach (GameObject range_child in unitclick)
+            {
+                Destroy(range_child);
+            }
+        }
     }
 
     // Start is called before the first frame update
     void Start()
     {
-       unitclick= Instantiate(unit_click)/* as GameObject*/;
+       //unitclick= Instantiate(unit_click)/* as GameObject*/;
 
     }
 
