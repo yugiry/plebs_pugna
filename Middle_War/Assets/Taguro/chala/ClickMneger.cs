0bb159c3f43b6edceece94f8c428bd695f369007
taguro82 <h1202419022@hiratagakuen.onmicrosoft.com>
2024-10-29 12:39:30 +0900

    ステータス表示を追加

---
+++ Middle_War/Assets/Taguro/chala/ClickMneger.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickMneger : MonoBehaviour
+{
+    public GameObject clickPanel
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        clickPanel.SetActive(false); //”ñ•\Ž¦
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
