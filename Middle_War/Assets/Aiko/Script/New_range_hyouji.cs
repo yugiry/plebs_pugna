929382b54cf90ecb8d32f9a6056f6d2262c787d2
Aiko-Dornan <h1202415010@hiratagakuen.onmicrosoft.com>
2024-10-30 10:39:03 +0900

    unity　攻撃範囲の改造完了

--- Middle_War/Assets/Aiko/Script/New_range_hyouji.cs
+++ Middle_War/Assets/Aiko/Script/New_range_hyouji.cs
@@ -8,35 +8,39 @@ public class New_range_hyouji : MonoBehaviour
     public Transform parent;
     public GameObject base_point_unit;
     private GameObject unitclick;
+    float masume_size=4.5f;
     public void Click_unit()
     {
         //unitclick = unit_click as GameObject; 
 
         Vector3 pos = parent.transform.localPosition;//クリックされたユニットの位置情報
-        if ( GameObject.Find("infantry"))
+        if (this.gameObject.name == "infantry")
         {
             Destroy_Range();
             Destroy_Range();
-            for (float x = - 4.0f; x <= +4.0f; x += 4.0f)
+            for (float x = -masume_size; x <= +masume_size; x += masume_size)
             {
-                for (float y = -4.0f; y <= +4.0f; y += 4.0f)
+                for (float y = -masume_size; y <= +masume_size; y += masume_size)
                 {
-                    unitclick = Instantiate(unit_click, new Vector3(pos.x+x, pos.y+y, -5.5f), Quaternion.identity, parent) as GameObject;
+                    unitclick = Instantiate(unit_click, new Vector3(pos.x + x, pos.y + y, -5.5f), Quaternion.identity, parent) as GameObject;
+
                 }
 
             }
-
             
+
             Debug.Log("クリックされたぞい。");
-             
-           // Destroy(unitclick, 1);
-        }else if (GameObject.Find("archer"))
+
+            // Destroy(unitclick, 1);
+        }
+        //弓兵処理
+        else if (this.gameObject.name == "archer")
         {
             Destroy_Range();
             Destroy_Range();
-            for (float x = -4.0f; x <= +4.0f; x += 4.0f)
+            for (float x = -masume_size * 2; x <= +masume_size * 2; x += masume_size)
             {
-                for (float y = -4.0f; y <= +4.0f; y += 4.0f)
+                for (float y = -masume_size * 2; y <= +masume_size * 2; y += masume_size)
                 {
                     unitclick = Instantiate(unit_click, new Vector3(pos.x + x, pos.y + y, -5.5f), Quaternion.identity, parent) as GameObject;
                 }
@@ -45,7 +49,37 @@ public class New_range_hyouji : MonoBehaviour
 
 
         }
+        //カタパルト処理
+        else if (this.gameObject.name == "catapult")
+        {
+            Destroy_Range();
+            Destroy_Range();
+            for (float x = -masume_size * 4; x <= +masume_size * 4; x += masume_size)
+            {
+                for (float y = -masume_size * 4; y <= +masume_size * 4; y += masume_size)
+                {
+                    if (x >= -masume_size * 1 && x <= masume_size * 1 && y >= -masume_size * 1 && y <= masume_size * 1)
+                    {
 
+                    }
+                    else
+                    {
+                        unitclick = Instantiate(unit_click, new Vector3(pos.x + x, pos.y + y, -5.5f), Quaternion.identity, parent) as GameObject;
+                    }
+                }
+
+
+
+
+            }
+
+
+
+
+
+
+
+        }
 
 
     }
@@ -73,6 +107,9 @@ public class New_range_hyouji : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButton(0))
+        {
+            Destroy_Range();
+        }
     }
 }
