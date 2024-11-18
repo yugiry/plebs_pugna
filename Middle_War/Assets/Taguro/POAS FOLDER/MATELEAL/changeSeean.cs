13ee775dbb2dd53cb2a813cb90142dbb6eb01b07
taguro82 <h1202419022@hiratagakuen.onmicrosoft.com>
2024-11-18 16:33:34 +0900

    タイトルと名前入力画面を作成（名前は保存できない）

---
+++ Middle_War/Assets/Taguro/POAS FOLDER/MATELEAL/changeSeean.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changeSeean : MonoBehaviour
+{
+    public void change_button()
+    {
+        SceneManager.LoadScene("NameSeean");
+    }
+}
---
+++ Middle_War/Assets/Taguro/POAS FOLDER/MATELEAL/taitleSeeanChange.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class taitleSeeanChange : MonoBehaviour
+{
+  public void change_button()
+    {
+        SceneManager.LoadScene("taitle");
+    }
+}
