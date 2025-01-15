2dca2d637d9599f659742de128de89a5b61e6eff
kirino-kazuto <h1202419004@hiratagakuen.onmicrosoft.com>
2025-01-15 11:30:57 +0900

    マップセレクト画面修正

--- Middle_War/Assets/Kirino/Scripts/mapfaito.cs
+++ Middle_War/Assets/Kirino/Scripts/mapfaito.cs
@@ -13,6 +13,7 @@ public class mapfaito : MonoBehaviour
     public GameObject mapbatoru;
     [SerializeField] public Text text1;//１つ目のテキスト変換
     [SerializeField] public Text text2;//２つ目のテキスト変換
+    //[SerializeField] enemynemaplate enemynemaplate;
     public Image image; //画像表示
     public GameObject Button;//敵とのバトルボタン
     public Sprite[] newSprite;//画像表示追加
@@ -37,49 +38,50 @@ public class mapfaito : MonoBehaviour
         {
             case 1:
                 text1.text = NameMneger.name+"の国"; //国名
-                text2.text = "どんどん敵国を攻め領土を拡張して天下統一を目指そう！！"; //国説明
+                text2.text = "どんどん敵国を攻め\n領土を拡張し\n天下統一を目指そう！"; //国説明/////
                 Button.SetActive(false); //ボタン表示（自国はボタン表示しない）
                 mapbatoru.transform.position = new Vector3(0, 0, 50); //国情報ボード表示位置
                 RCN.country_num = countrynum;
                 break;
             case 2:
                 text1.text = "フスラン"; //国名
-                text2.text = "できたばかりの国で国王の権力が低く攻め入りやすい！"; //国説明            
+                text2.text = "できたばかりの国で\n国王の権力が弱く\n攻め入りやすい"; //国説明  /////    
                 Button.SetActive(true); //敵国はボタン表示する
                 mapbatoru.transform.position = new Vector3(0, 0, 50); //国情報ボード表示位置
                 RCN.country_num = countrynum;
+               
                 break;
             case 3:
                 text1.text = "ア・タイリ"; //国名
-                text2.text = "できたばかりではないが国民のまとまりがなくあまり発展していない！"; //国説明             
+                text2.text = "少し前にできた国だが\n国民のまとまりがなく\nあまり発展していない"; //国説明  ////           
                 Button.SetActive(true); //敵国はボタン表示する
                 mapbatoru.transform.position = new Vector3(0, 0, 50); //国情報ボード表示位置
                 RCN.country_num = countrynum;
                 break;
             case 4:
                 text1.text = "アイべり半島"; //国名
-                text2.text = "少し発展しており徐々に宗教が広まっている!!（宗教名ligare）"; //国説明          
+                text2.text = "少し発展しており\n徐々に宗教が広まり\n国民のまとまりがある"; //国説明    ///      
                 Button.SetActive(true); //敵国はボタン表示する
                 mapbatoru.transform.position = new Vector3(0, 0, 50); //国情報ボード表示位置
                 RCN.country_num = countrynum;
                 break;
             case 5:
                 text1.text = "アベチネ";
-                text2.text = "国自体は狭いが漁業が盛んでいろんな国と取引している。";
+                text2.text = "国自体は狭いが漁業が\n盛んでいろんな国と\n取引している";//国説明////
                 Button.SetActive(true); //敵国はボタン表示する
                 mapbatoru.transform.position = new Vector3(0, 0, 50); //国情報ボード表示位置
                 RCN.country_num = countrynum;
                 break;
             case 6:
                 text1.text = "ナクウイラ";
-                text2.text = "どの国からも狙われやすいが、攻めずらい。";
+                text2.text = "他国に狙われやすいが\n地形が入り組んでいて\n攻めずらい国";//国説明////
                 Button.SetActive(true); //敵国はボタン表示する
                 mapbatoru.transform.position = new Vector3(0, 0, 50); //国情報ボード表示位置
                 RCN.country_num = countrynum;
                 break;
             case 7:
                 text1.text = "アブリガル";
-                text2.text = "アイべり半島と対立しており少しずつリガル教が進行している。";
+                text2.text = "アイベリ半島と対立\nしており宗教が徐々に\n進行してきている";//国説明////
                 Button.SetActive(true); //敵国はボタン表示する
                 mapbatoru.transform.position = new Vector3(0, 0, 50); //国情報ボード表示位置
                 RCN.country_num = countrynum;
@@ -93,5 +95,6 @@ public class mapfaito : MonoBehaviour
     public void change_button()//ボタン変更
     {
         SceneManager.LoadScene(NextScene);//ボタンが押された時シーンを変える
+        //enemynemaplate.Show_country(countrynum);
     }
 }
