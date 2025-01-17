a7223d182ade2caa70d6be211bdfff8d2e8c78d1
kirino-kazuto <h1202419004@hiratagakuen.onmicrosoft.com>
2025-01-17 11:32:48 +0900

    ポーズ画面変更

---
+++ Middle_War/Assets/alpha/Script/restert.cs
@@ -0,0 +1,14 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class restert : MonoBehaviour
+{
+    public GameObject restertUI;
+    public void Cllik()
+    {
+        restertUI.SetActive(false);
+    }
+  
+}
