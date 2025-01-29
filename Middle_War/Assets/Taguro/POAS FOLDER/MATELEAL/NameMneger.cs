8afc61126fdb992253ddd33ead7bbaa3b7ffa9ac
taguro82 <h1202419022@hiratagakuen.onmicrosoft.com>
2025-01-29 16:32:13 +0900

    NameMnegerにドキュメントコメントを追加

--- Middle_War/Assets/Taguro/POAS FOLDER/MATELEAL/NameMneger.cs
+++ Middle_War/Assets/Taguro/POAS FOLDER/MATELEAL/NameMneger.cs
@@ -4,7 +4,9 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
-
+/// <summary>
+/// 名前を入力をしてからテキストを獲得するためのもの
+/// </summary>
 public class NameMneger : MonoBehaviour
 {
     public GameObject NameEnter;
@@ -15,6 +17,7 @@ public class NameMneger : MonoBehaviour
     //説明
     public void GetText()
     {
+   
         Text input_text = GameObject.Find("name_entry").GetComponent<Text>();//name_entryに入力されたのを確認する
 
         string name_text = input_text.text.Replace("\n","").Replace(" ","").Replace(" ","").Trim();//名前が入力されていない・空白がある場合ボタンを押せないようにする
